a26f7001f1c225890ad7b4dba3b3e929b3ec2e8f
Itadori3440 <75749808+Itadori3440@users.noreply.github.com>
2020-12-09 20:50:53 +0200

    Add files via upload

---
+++ laba1/zavd2.m
@@ -0,0 +1,5 @@
+k = 23;
+array1 = [k 17 -7+k; -k 3*k 1; k+2 7 k+8]
+det(array1)
+array2 = [2-k 4*k; k 6] 
+det(array2)
---
+++ laba1/zavd3.m
@@ -0,0 +1,7 @@
+k = 23;
+array1 = [3 4; 5 k]
+inv(array1)
+array2 = [2 k+1 k; 6 3 4; 5 -2 k]
+inv(array2)
+array3 = [1 2*k -1; 3 0 2; k-2 -2 5-k]
+inv(array3)
